2680992091422fb57a031548f77ecc84dfb54a0d
JhoerV <134255588+JhoerV@users.noreply.github.com>
2026-05-04 00:29:22 -0500

    Se subio el laboratorio 03

---
+++ Lab03/Codigo/EspadaGolpe.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class EspadaGolpe : MonoBehaviour
+{
+    public float fuerzaGolpe;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Cubo"))
+        {
+            Rigidbody rb = collision.gameObject.GetComponent<Rigidbody>();
+
+            if (rb != null)
+            {
+                Vector3 direccion = collision.transform.position - transform.position;
+
+                rb.AddForce(direccion.normalized * fuerzaGolpe, ForceMode.Impulse);
+            }
+        }
+    }
+}
---
+++ Lab03/Codigo/ataque.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class ataque : MonoBehaviour
+{
+    public float angulo = 90f;
+    public float velocidad = 360f;
+    public KeyCode teclaAtaque = KeyCode.Space;
+
+    private bool atacando = false;
+    private float rotado = 0f;
+    private Quaternion rotacionInicial;
+
+    void Start()
+    {
+        rotacionInicial = transform.rotation;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(teclaAtaque) && !atacando)
+        {
+            atacando = true;
+            rotado = 0f;
+        }
+
+        if (atacando)
+        {
+            float paso = velocidad * Time.deltaTime;
+
+            transform.Rotate(0f, 0f, -paso);
+            rotado += paso;
+
+            if (rotado >= angulo)
+            {
+                atacando = false;
+                transform.rotation = rotacionInicial;
+            }
+        }
+    }
+}
